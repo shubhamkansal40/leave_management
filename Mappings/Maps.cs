673071a0e01860002846e92f2f1825d2770863aa
Shubham Kansal <shubhamkansal40@yahoo.co.in>
2020-06-12 13:52:52 +0530

    first

---
+++ Mappings/Maps.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leave_management.Models;
+using leave_management.Data;
+
+namespace leave_management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType,DetailsLeaveTypeVM>().ReverseMap();           
+            CreateMap<LeaveType,CreateLeaveTypeVM>().ReverseMap();           
+            CreateMap<LeaveHistory,LeaveHistoryVM>().ReverseMap();           
+            CreateMap<LeaveAllocation,LeaveAllocationVM>().ReverseMap();
+            CreateMap<Employee,EmployeeVM>().ReverseMap();
+
+        }
+    }    
+}
---
+++ Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,52 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            var leaveAllocation = _db.LeaveAllocations.ToList();
+            return leaveAllocation;
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            var leaveAllocation = _db.LeaveAllocations.Find(id);
+            return leaveAllocation;
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.Update(entity);
+            return Save();
+        }
+    }
+}
