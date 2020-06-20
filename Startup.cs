c1a06faeee700004bde0be6655ef1e119030422e
Shubham Kansal <shubhamkansal40@yahoo.co.in>
2020-06-20 10:44:26 +0530

    Test

--- Startup.cs
+++ Startup.cs
@@ -25,11 +25,11 @@ namespace leave_management
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration  Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {//test
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
